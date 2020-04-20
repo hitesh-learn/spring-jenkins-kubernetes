9f0395ac017f63e656d2126fa7585dbf731ac4ec
Hitesh <hitesh.joshi@consensys.net>
2020-04-21 01:57:25 +0800

    CI/Cd deploy to docker

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test() {
-        return "Test 1";
+        return "Testing CI /CD";
     }
 }
